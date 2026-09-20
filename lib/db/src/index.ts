92e7d68353ff6161655884e4dceb2dff6467643e
hatukay26 <ibraheemhat1@gmail.com>
2026-09-20 20:48:33 +0300

    Update schema export path in index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-export * from "./schema";
+export * from "./schema/index";
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
