9b347c3ef0268cd64de248dcb19e35ae4e39285f
hatukay26 <ibraheemhat1@gmail.com>
2026-09-20 20:48:05 +0300

    Update index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "./schema";
-
+export * from "./schema";
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
