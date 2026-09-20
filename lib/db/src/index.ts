c13edc3573b1297fe6875b1f88c232a8cbcc3885
hatukay26 <ibraheemhat1@gmail.com>
2026-09-20 20:55:51 +0300

    Refactor database export and import schema

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
+import * as schema from "./schema/index";
+
 export * from "./schema/index";
+
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
@@ -11,5 +14,3 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
-
-export * from "./schema";
