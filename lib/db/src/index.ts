3f9bc204ef386da8104d02b446fe7e470c28203a
hatukay26 <ibraheemhat1@gmail.com>
2026-09-21 10:02:53 +0300

    Change schema import to customers module

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,8 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "./schema/index.js";
+import * as schema from "./schema/customers";
 
-export * from "./schema/index.js";
+export * from "./schema/customers";
 
 const { Pool } = pg;
 
