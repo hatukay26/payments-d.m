cfb10ce3aff2a922a6967ebb26856ce56f48c0d9
hatukay26 <ibraheemhat1@gmail.com>
2026-09-23 11:16:13 +0300

    Update export path for customers schema

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -2,8 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "./schema/customers";
 
-export * from "./schema/customers";
-
+export * from "./customers";
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
